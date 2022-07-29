6abe40a3afa5d30cf94e8063d5d950a9136058fe
FrancoMartinPersonal <francomartin.personal@gmail.com>
2022-07-29 20:26:28 -0300

    driver chrome

---
+++ Src/driver/driver_chrome.py
@@ -0,0 +1,15 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+options = Options()
+
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1200")
+options.add_argument("--ignore-certificate-errors")
+options.add_argument("--disk-cache-size=0")
+options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
